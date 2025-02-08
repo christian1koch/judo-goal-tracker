88ec8abcd57d7b9164173aed6173852d84664c4a
Christian Koch Echeverria <ko.che@web.de>
2025-02-08 22:01:36 +0100

    fix sign in layouting

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,9 +1,13 @@
 export default async function Layout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
-  );
+	return (
+		<div className="flex justify-center align-middle items-center h-full">
+			<div className="max-w-7xl flex flex-col justify-center gap-12 items-start">
+				{children}
+			</div>
+		</div>
+	);
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -41,8 +41,8 @@ export default function RootLayout({
 						enableSystem
 						disableTransitionOnChange
 					>
-						<main className="min-h-screen flex flex-col items-center">
-							<div className="flex-1 w-full flex flex-col items-center">
+						<main className="min-h-screen h-screen flex flex-col items-center">
+							<div className="flex-1 w-full flex flex-col items-center h-full">
 								<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
 									<div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
 										{!hasEnvVars ? (
@@ -59,7 +59,7 @@ export default function RootLayout({
 									{children}
 								</div>
 
-								<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8">
+								<footer className="w-full bottom-0 fixed flex items-center justify-center border-t mx-auto text-center text-xs gap-8">
 									<p>
 										Powered by{" "}
 										<a
