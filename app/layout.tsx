f541001de7c8a628bc9383620582cda1c7aa07c6
Christian Koch Echeverria <ko.che@web.de>
2025-02-09 16:24:54 +0100

    added anti aliased to the text

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 						enableSystem
 						disableTransitionOnChange
 					>
-						<main className="min-h-screen h-screen flex flex-col items-center">
+						<main className="min-h-screen h-screen flex flex-col items-center antialiased">
 							<div className="flex-1 w-full flex flex-col items-center h-full">
 								<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
 									<div className="w-full max-w-5xl flex justify-end items-center p-3 px-5 text-sm">
