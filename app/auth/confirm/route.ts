8ab01d1a2c74ac9fd0406585301e24ad1f72c25a
Christian Koch Echeverria <ko.che@web.de>
2025-02-09 10:26:35 +0100

    test redirect phone

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -2,7 +2,7 @@ import { type EmailOtpType } from "@supabase/supabase-js";
 import { type NextRequest } from "next/server";
 
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export async function GET(request: NextRequest) {
 	const { searchParams } = new URL(request.url);
@@ -19,10 +19,10 @@ export async function GET(request: NextRequest) {
 		});
 		if (!error) {
 			// redirect user to specified redirect URL or root of app
-			redirect("/protected");
+			// redirect("/protected");
 		}
 	}
 
 	// redirect the user to an error page with some instructions
-	redirect("/error");
+	// redirect("/error");
 }
