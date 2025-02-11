6bc68bb5ae4cbcda31760c0cbeef91e30c6d3cd6
Christian Koch Echeverria <ko.che@web.de>
2025-02-11 14:48:32 +0100

    #3: disabled note while create

--- components/notes/create-new-note-button.tsx
+++ components/notes/create-new-note-button.tsx
@@ -2,20 +2,24 @@
 import { createNote } from "@/app/actions";
 import { Button } from "@heroui/react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function CreateNewNoteButton() {
 	const router = useRouter();
 	const onCreateNote = async () => {
 		// TODO: try catch
+		setIsCreating(true);
 		const newNote = await createNote("", "");
 		router.push(`/protected/notes/${newNote.id}`);
 	};
+	const [isCreating, setIsCreating] = useState(false);
 	return (
 		<Button
 			className="flex-grow-0"
 			color="primary"
 			variant="light"
 			onPress={onCreateNote}
+			isDisabled={isCreating}
 		>
 			Create New Note
 		</Button>
